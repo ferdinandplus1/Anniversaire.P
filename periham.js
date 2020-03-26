9fd7e4432d5210b4f501856b9ad5619e85436db0
ferdinandpus1 <zgferdinando@yahoo.fr>
2020-03-26 16:28:59 +0100

    first version of periham site

---
+++ periham.js
@@ -0,0 +1,19 @@
+
+$(document).ready(function(){
+  $("#trt").on({
+    /*mouseenter:function(){
+      $("#trt").hide(2000);
+     },
+     mouseleave:function(){
+      $("#trt").show(2000);
+     },*/
+     click:function(){
+      $("#trt").toggle();
+     }
+   
+
+  });
+
+       
+                           
+                });   
